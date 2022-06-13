da60c1684bb8b295a871d3cb82734e78ac692770
ntabucejo <ntabucejo@gmail.com>
2022-06-13 17:28:16 +0800

    Initial game commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,39 +1,139 @@
-import logo from './public/logo.svg'
+import React, { useEffect, useState } from 'react'
+import * as HeroOutline from '@heroicons/react/outline'
+import { motion } from 'framer-motion'
 
 const App = () => {
+  const [tiles, setTiles] = useState(Array(9).fill(''))
+  const [hasWinner, setHasWinner] = useState(false)
+  const [winnerTiles, setWinnderTiles] = useState<number[]>([])
+
   return (
-    <div className="grid min-h-screen justify-items-center bg-gray-900 p-24 text-2xl text-white">
-      <div className="grid place-content-center text-center">
-        <img
-          src={logo}
-          alt="logo"
-          className="mx-auto w-[100%] animate-bounce"
+    <div className="grid h-screen place-items-center">
+      <div className="relative space-y-10">
+        <Board
+          tiles={tiles}
+          setTiles={setTiles}
+          hasWinner={hasWinner}
+          setHasWinner={setHasWinner}
+          winnerTiles={winnerTiles}
+          setWinnderTiles={setWinnderTiles}
         />
-        <div className="space-y-4">
-          <p>
-            Edit <code>App.tsx</code> and save to test HMR updates.
-          </p>
-          <p>
-            <a
-              className="text-sky-500 underline hover:text-sky-400"
-              href="https://reactjs.org"
-              target="_blank"
-              rel="noopener noreferrer">
-              Learn React
-            </a>
-            {' | '}
-            <a
-              className="text-sky-500 underline hover:text-sky-400"
-              href="https://vitejs.dev/guide/features.html"
-              target="_blank"
-              rel="noopener noreferrer">
-              Vite Docs
-            </a>
-          </p>
-        </div>
+        {hasWinner && (
+          <button
+            onClick={() => {
+              setTiles(Array(9).fill(''))
+              if (hasWinner) {
+                setHasWinner(false)
+                setWinnderTiles([])
+              }
+            }}
+            className="absolute w-full rounded-lg bg-blue-500 px-4 py-2 text-2xl text-white">
+            RESET
+          </button>
+        )}
       </div>
     </div>
   )
 }
 
 export default App
+
+const Board = ({
+  tiles,
+  setTiles,
+  hasWinner,
+  setHasWinner,
+  winnerTiles,
+  setWinnderTiles,
+}: {
+  tiles: string[]
+  setTiles: React.Dispatch<React.SetStateAction<string[]>>
+  hasWinner: boolean
+  setHasWinner: React.Dispatch<React.SetStateAction<boolean>>
+  winnerTiles: number[]
+  setWinnderTiles: React.Dispatch<React.SetStateAction<number[]>>
+}) => {
+  const [player, setPLayer] = useState('x')
+  const [conditions] = useState([
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ])
+
+  useEffect(() => {
+    for (const condition of conditions) {
+      if (
+        tiles[condition[0]] === tiles[condition[1]] &&
+        tiles[condition[1]] === tiles[condition[2]] &&
+        tiles[condition[0]] &&
+        tiles[condition[1]] &&
+        tiles[condition[2]]
+      ) {
+        setHasWinner(true)
+        if (hasWinner) {
+          setWinnderTiles(condition)
+        }
+      }
+    }
+  }, [tiles, hasWinner])
+
+  const handleMove = (index: number) => {
+    const updatedTiles = tiles.map((tile, i) => {
+      if (i !== index || tile) return tile
+      if (!hasWinner) {
+        setPLayer(player === 'x' ? 'o' : 'x')
+      }
+      return player
+    })
+    if (!hasWinner) {
+      setTiles(updatedTiles)
+    }
+  }
+
+  return (
+    <div className="group grid grid-cols-3 gap-1">
+      {tiles.map((tile, index) => (
+        <button
+          key={index}
+          onClick={() => handleMove(index)}
+          className={`h-44 w-44 bg-slate-200`}>
+          {tile === 'x' && (
+            <motion.div
+              initial={{ scale: 1.5 }}
+              animate={{ scale: 1 }}
+              transition={{ duration: 0.25 }}
+              whileTap={{ scale: 1.2 }}>
+              <HeroOutline.XIcon
+                className={`w-full ${
+                  winnerTiles[0] === index && 'text-green-500'
+                } ${winnerTiles[1] === index && 'text-green-500'} ${
+                  winnerTiles[2] === index && 'text-green-500'
+                }`}
+              />
+            </motion.div>
+          )}
+          {tile === 'o' && (
+            <motion.div
+              initial={{ scale: 1.5 }}
+              animate={{ scale: 1 }}
+              transition={{ duration: 0.25 }}
+              whileTap={{ scale: 1.2 }}>
+              <HeroOutline.MinusCircleIcon
+                className={`w-full ${
+                  winnerTiles[0] === index && 'text-green-500'
+                } ${winnerTiles[1] === index && 'text-green-500'} ${
+                  winnerTiles[2] === index && 'text-green-500'
+                }`}
+              />
+            </motion.div>
+          )}
+        </button>
+      ))}
+    </div>
+  )
+}
